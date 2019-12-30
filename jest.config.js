065e8179094de21bce74799fd49af1c6350ab4df
Katsuhito Iwai <bruin.iwai@gmail.com>
2019-12-30 13:54:35 +0900

    add auth & change my favorite lint rules

--- jest.config.js
+++ jest.config.js
@@ -2,16 +2,17 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     '^~/(.*)$': '<rootDir>/$1',
-    '^vue$': 'vue/dist/vue.common.js'
+    '^vue$': 'vue/dist/vue.common.js',
   },
   moduleFileExtensions: ['js', 'vue', 'json'],
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
+    '.*\\.(vue)$': 'vue-jest',
   },
   collectCoverage: true,
+  // eslint-disable-next-line prettier/prettier
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
     '<rootDir>/pages/**/*.vue'
   ]
-}
+};
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
-import colors from 'vuetify/es5/util/colors'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import colors from 'vuetify/es5/util/colors';
 
 export default {
   mode: 'spa',
@@ -6,7 +7,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
+    titleTemplate: `%s - ${process.env.npm_package_name}`,
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
@@ -14,10 +15,10 @@ export default {
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
-      }
+        content: process.env.npm_package_description || '',
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
   /*
    ** Customize the progress-bar color
@@ -37,7 +38,7 @@ export default {
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
-    '@nuxtjs/vuetify'
+    '@nuxtjs/vuetify',
   ],
   /*
    ** Nuxt.js modules
@@ -46,7 +47,8 @@ export default {
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     // Doc: https://github.com/nuxt-community/dotenv-module
-    '@nuxtjs/dotenv'
+    '@nuxtjs/dotenv',
+    '@nuxtjs/auth',
   ],
   /*
    ** Axios module configuration
@@ -69,10 +71,10 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+          success: colors.green.accent3,
+        },
+      },
+    },
   },
   /*
    ** Build configuration
@@ -81,6 +83,7 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
-  }
-}
+    // eslint-disable-next-line no-unused-vars
+    extend(config, ctx) {},
+  },
+};
