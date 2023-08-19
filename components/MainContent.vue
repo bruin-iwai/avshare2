71cfdf212516f994195d20b940822377b7999211
Katsuhito Iwai <bruin.iwai@gmail.com>
2023-08-19 07:03:31 +0000

    vuetify3 正式対応

--- components/MainContent.vue
+++ components/MainContent.vue
@@ -1,65 +1,57 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <v-card title="My favorite videos">
-        <template #text>
-          <v-select v-model="vmPrefix" :items="possiblePrefixes" label="prefix" outlined dense />
-          <v-list :items="urls" item-title="title" item-value="url">
-            <template #item="{ props }">
-              <a :href="props.value" target="_blank">{{ props.title }}</a>
-            </template>
-          </v-list>
-        </template>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-container fluid>
+    <v-row no-gutters>
+      <v-col align-self="center">
+        <v-select v-model="prefix" :items="['', 'my-favorites', 'old-programs']" label="prefix" />
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col align-self="center">
+        <v-card v-if="prefix">
+          <v-card-text>
+            <v-list>
+              <v-list-item v-for="(item, i) in urls" :key="i">
+                <v-list-item-title>
+                  <a :href="item.url" target="_blank">{{ item.title }}</a>
+                </v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup lang="ts">
+// declare interfaces
 interface UrlAndTitle {
   url: string;
   title: string;
 }
 
-interface UrlAndTitleMap {
-  [key: string]: UrlAndTitle[];
-}
+// get config
+const {
+  public: { baseURL, apiKey },
+} = useRuntimeConfig();
 
 // states
-const possiblePrefixes = ref(['', 'my-favorites', 'old-programs']);
 const prefix = ref('');
-const urlAndTitleMap = ref<UrlAndTitleMap>({});
+const urls = ref<UrlAndTitle[]>([]);
 
-// getters
-const urls = computed<UrlAndTitle[]>(() => urlAndTitleMap.value[prefix.value]);
-const vmPrefix = computed<string>({
-  get() {
-    return prefix.value;
-  },
-  set(newValue) {
-    if (newValue === prefix.value) {
-      return;
-    }
-    prefix.value = newValue;
+// watch effects
+watchEffect(async () => {
+  // guard condition
+  if (!prefix.value) {
+    return;
+  }
 
-    if (newValue !== '') {
-      changePrefix(newValue);
-    }
-  },
-});
-
-// actions
-const changePrefix = async (newPrefix: string): Promise<void> => {
-  const config = useRuntimeConfig();
-  const { baseURL, apiKey } = config.public;
-  const result = await $fetch<UrlAndTitle[]>(`/dev/contents/${newPrefix}`, {
+  const result = await $fetch<UrlAndTitle[]>(`/dev/contents/${prefix.value}`, {
     baseURL,
     headers: {
       'x-api-key': apiKey,
     },
   });
-
-  prefix.value = newPrefix;
-  urlAndTitleMap.value[newPrefix] = result;
-};
+  urls.value = result;
+});
 </script>
